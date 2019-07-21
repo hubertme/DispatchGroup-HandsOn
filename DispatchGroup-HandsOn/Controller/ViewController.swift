773bfda7695daf67b362c8effa363563b8f5dc0b
Hubert Wang <hubert.tatra@gmail.com>
2019-07-21 19:32:25 +0700

    Add centre image view for testing fetch image

--- DispatchGroup-HandsOn/Controller/ViewController.swift
+++ DispatchGroup-HandsOn/Controller/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var contentTableView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.testImageFetch()
+    }
+    
+    private func testImageFetch() {
+        Networking.shared.fetchImageFromServer(stringUrl: "https://images.pexels.com/photos/2668676/pexels-photo-2668676.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", successCompletion: { (image) in
+            self.imageView.image = image
+        }) {
+            print("Fail to load image")
+        }
     }
 }
 
